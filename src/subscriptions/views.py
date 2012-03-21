a5cff434d80a6a53492fb0b4bc4c650eb43a2643
Marcelo Donato <mrdonatao@gmail.com>
2012-03-21 08:33:48 -0300

    Refactory 1

--- src/subscriptions/views.py
+++ src/subscriptions/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.core.mail import send_mail
+from django.conf import settings
+from models import Subscription
 from forms import SubscriptionForm
 
-def subscribe(request):
-    form = SubscriptionForm()
-    context = RequestContext(request, {'form': form})
-    return render_to_response('subscriptions/new.html', context)
+def subscribe(request, template_name='subscriptions/new.html'):
+    form = SubscriptionForm(request.POST or None)
+    if form.is_valid():
+        subscription = form.save()
+        send_mail(subject=u'Cadastrado com sucesso',
+                  message=u'Obrigado pela sua inscricao',
+                  from_email=settings.DEFAULT_FROM_EMAIL,
+                  recipient_list=[subscription.email]
+        )
+        return HttpResponseRedirect(
+        reverse('subscriptions:success', args=[ subscription.pk ]))
+ 
+    
+    return render(request, template_name, {'form':form})
+
+def success(request, id, template_name='subscriptions/success.html'):
+    subscription = get_object_or_404(Subscription, pk=id)
+    return render(request, template_name, {'subscription':subscription})
+
